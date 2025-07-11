9c9922029ec11b7f485b0ff63006956d3923b76c
Leandro Cunha <leandrocunha016@gmail.com>
2025-07-11 20:29:17 -0300

    refactor: replace GridView with MasonryGridView for improved layout and add flutter_staggered_grid_view dependency

--- lib/widgets/photo_mosaic.dart
+++ lib/widgets/photo_mosaic.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class PhotoMosaic extends StatelessWidget {
   final List<File> images;
@@ -12,17 +13,42 @@ class PhotoMosaic extends StatelessWidget {
       return const Center(child: Text("Nenhuma foto adicionada"));
     }
 
-    return GridView.builder(
-      padding: const EdgeInsets.all(8),
-      itemCount: images.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // ajusta para 2 colunas
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: MasonryGridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          final file = images[index];
+          final image = Image.file(file);
+          return FutureBuilder<Size>(
+            future: _getImageSize(file),
+            builder: (context, snapshot) {
+              double height = 100;
+              if (snapshot.hasData) {
+                final size = snapshot.data!;
+                height = size.width > size.height ? 80 : 160;
+              }
+              return Container(
+                height: height,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.shade400),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                clipBehavior: Clip.antiAlias,
+                child: image,
+              );
+            },
+          );
+        },
       ),
-      itemBuilder: (context, index) {
-        return Image.file(images[index], fit: BoxFit.cover);
-      },
     );
+  }
+
+  Future<Size> _getImageSize(File file) async {
+    final decoded = await decodeImageFromList(await file.readAsBytes());
+    return Size(decoded.width.toDouble(), decoded.height.toDouble());
   }
 }
